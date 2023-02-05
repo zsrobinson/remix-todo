1e05ee857aa541859180b304dfc45b070a36f482
Zachary Robinson <me@zsrobinson.com>
2023-02-04 23:23:32 -0500

    Clear all completed items button

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,23 +4,36 @@ import {
   IconSquareRounded,
   IconSquareRoundedCheck,
   IconSquareRoundedPlus,
+  IconTrash,
   IconX,
 } from "@tabler/icons-react";
 import { useEffect, useRef } from "react";
 import { prisma } from "~/db.server";
 
 export async function loader({}: LoaderArgs) {
-  let todos = await prisma.todos.findMany();
-  return json(todos);
+  let todos = await prisma.todos.findMany({
+    orderBy: {
+      completed: "asc",
+    },
+  });
+
+  let completedItems = await prisma.todos.count({
+    where: {
+      completed: true,
+    },
+  });
+
+  return json({ todos, completedItems });
 }
 
 export default function Index() {
-  const todos = useLoaderData<typeof loader>();
+  const { todos, completedItems } = useLoaderData<typeof loader>();
 
   const transition = useTransition();
-  const isAdding =
-    transition.state === "submitting" &&
-    transition.submission.formData.get("_action") === "add";
+  const isAdding = transition.submission?.formData.get("_action") === "add";
+  const isRemoving =
+    transition.submission?.formData.get("_action") === "remove";
+
   let addFormRef = useRef<HTMLFormElement>(null);
   useEffect(() => {
     // triggers after adding a todo
@@ -31,10 +44,31 @@ export default function Index() {
 
   return (
     <main className="m-4">
-      <h2 className="text-xl font-semibold mb-4">Default Todo List</h2>
+      <div className="mb-4 flex items-center gap-4">
+        <h2 className="text-xl font-semibold">Default Todo List</h2>
+        {completedItems > 0 && (
+          <Form method="post">
+            <button
+              type="submit"
+              name="_action"
+              value="clear"
+              className="flex items-center gap-1 rounded-md bg-zinc-800 py-1 px-2 text-xs text-zinc-400"
+            >
+              <IconTrash size={14} />
+              Clear Completed Items
+            </button>
+          </Form>
+        )}
+      </div>
+
       <ul className="flex flex-col gap-2">
         {todos.map((todo) => (
-          <li key={todo.id} className="flex gap-2 items-center">
+          <li
+            key={todo.id}
+            className={`flex items-center gap-2 ${
+              todo.completed && "text-zinc-500 line-through"
+            }`}
+          >
             <Form method="post" className="flex items-center">
               <input type="hidden" name="id" value={todo.id} />
               <input
@@ -55,8 +89,16 @@ export default function Index() {
 
             <Form method="post" className="flex items-center">
               <input type="hidden" name="id" value={todo.id} />
-              <button type="submit" name="_action" value="remove">
-                <IconX size={16} className="text-zinc-500 mt-[2px]" />
+              <button
+                type="submit"
+                name="_action"
+                value="remove"
+                disabled={
+                  isRemoving &&
+                  todo.id === transition.submission?.formData.get("id")
+                }
+              >
+                <IconX size={16} className="mt-[2px] text-zinc-500" />
               </button>
             </Form>
           </li>
@@ -65,15 +107,20 @@ export default function Index() {
         <li>
           <Form
             method="post"
-            className="flex gap-2 items-center"
+            className="flex items-center gap-2"
             ref={addFormRef}
           >
-            <button type="submit" name="_action" value="add">
+            <button
+              type="submit"
+              name="_action"
+              value="add"
+              disabled={isAdding}
+            >
               <IconSquareRoundedPlus />
             </button>
             <input
               name="title"
-              className="bg-zinc-900 border-zinc-500 border-b focus:border-zinc-300 focus:outline-0"
+              className="border-b border-zinc-500 bg-zinc-900 focus:border-zinc-300 focus:outline-0"
             />
           </Form>
         </li>
@@ -94,6 +141,10 @@ export async function action({ request }: ActionArgs) {
   if (action === "remove") {
     const id = formData.get("id") as string;
     await prisma.todos.delete({ where: { id } });
+  }
+
+  if (action === "clear") {
+    return await prisma.todos.deleteMany({ where: { completed: true } });
   }
 
   if (action === "toggle") {
