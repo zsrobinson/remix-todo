dd7ef43271651d04798a42f581acb7e3ea1c86e1
Zachary Robinson <me@zsrobinson.com>
2023-02-06 19:03:27 -0500

    Initialize database migration

---
+++ prisma/migrations/20230207000029_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Todos" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
