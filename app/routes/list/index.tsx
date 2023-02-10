3886a2825e84fc6aa2417dd94c012d347c85e008
Zachary Robinson <me@zsrobinson.com>
2023-02-10 15:22:50 +0000

    Resolves No Default List #9

--- app/routes/list/index.tsx
+++ app/routes/list/index.tsx
@@ -3,8 +3,10 @@ import { prisma } from "~/db.server";
 
 export async function loader({}: LoaderArgs) {
   const firstList = await prisma.list.findFirst();
+  if (firstList) return redirect(`/list/${firstList.id}`);
 
-  if (!firstList) throw new Response("No Lists Found", { status: 404 });
-
-  return redirect(`/list/${firstList.id}`);
+  const newList = await prisma.list.create({
+    data: { title: "Default List", color: "gray" },
+  });
+  return redirect(`/list/${newList.id}`);
 }
